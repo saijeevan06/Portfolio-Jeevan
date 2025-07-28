11be712c3de0d177703847f74d0307c08aa6d8bc
Jeevan <makamsaijeevan06@gmail.com>
2025-07-28 10:41:50 +0530

    updated

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './portfolio.css';
+import './Portfolio.css';
 
 const Portfolio = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
